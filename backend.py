a40e2873481447827da62d9c660610c8a69b960c
leekempson <leekempson@hotmail.com>
2024-10-24 12:38:05 +0200

    changed api to represent celcius

--- backend.py
+++ backend.py
@@ -2,10 +2,8 @@ import requests
 
 API_KEY = "6ff8ff21eb30ff5c8581dbc331b8f893"
 
-https://api.openweathermap.org/data/2.5/forecast?q={place}&units=metric&appid={API_KEY}
-
 def get_data(place, forecast_days):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&units=metric&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
 
@@ -16,8 +14,7 @@ def get_data(place, forecast_days):
     return filtered_data
 
 
-#get data is only triggered when used directly and not imported anywhere else
-
+#get data is only triggered when used directly from this file
 
 if __name__ =="__main__":
     print(get_data(place="Manchester", forecast_days=1,))
--- main.py
+++ main.py
@@ -15,19 +15,22 @@ st.subheader(f"{option} for the next {days} days in {place}")
 
 #if place is provided execute below (avoids error when app is first started as no place is provided
 if place:
-#get the temp/sky data
-filtered_data = get_data(place=place, forecast_days=days)
 
-if option == "Temperature":
-    temperatures = [dict["main"]["temp"] for dict in filtered_data]
-    dates = [dict["dt_txt"] for dict in filtered_data]
-    #create a temp plot
-    figure = px.line(x=dates, y=temperatures,
-                     labels={"x": "Date", "y": "Temperature (C)"})
-    st.plotly_chart(figure)
+    #get the temp/sky data
+    filtered_data = get_data(place=place, forecast_days=days)
 
-if option == "Sky":
-    # note that weather is a list containing one dict thereofre we reference the index of that single dict with [0].
-    # The main key holds required key, eg Rain, Cloud etc..
-    filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
-    st.image()
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] for dict in filtered_data]
+
+        #note dt is the key which will give you raw date and dt_txt coverts to format like '2024-10-24 12:00:00' _
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        #create a temp plot
+        figure = px.line(x=dates, y=temperatures,
+                         labels={"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
+
+    if option == "Sky":
+        # note that weather is a list containing one dict thereofre we reference the index of that single dict with [0].
+        # The main key holds required key, eg Rain, Cloud etc..
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+        st.image()
