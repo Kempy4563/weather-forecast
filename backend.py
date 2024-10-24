fd842993d291a772c4b154931eac56226eb6b646
leekempson <leekempson@hotmail.com>
2024-10-24 12:23:13 +0200

    added temperature graph

--- backend.py
+++ backend.py
@@ -2,16 +2,25 @@ import requests
 
 API_KEY = "6ff8ff21eb30ff5c8581dbc331b8f893"
 
+https://api.openweathermap.org/data/2.5/forecast?q={place}&units=metric&appid={API_KEY}
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+
+    return filtered_data
+
 
 #get data is only triggered when used directly and not imported anywhere else
+
+
 if __name__ =="__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Manchester", forecast_days=1,))
 
 
 
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import traceback
+from backend import get_data
 
 import streamlit as st
 import plotly.express as px
 
+# widgets
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -11,11 +13,21 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-temperatures = [10 ,11, 15]
+#if place is provided execute below (avoids error when app is first started as no place is provided
+if place:
+#get the temp/sky data
+filtered_data = get_data(place=place, forecast_days=days)
 
+if option == "Temperature":
+    temperatures = [dict["main"]["temp"] for dict in filtered_data]
+    dates = [dict["dt_txt"] for dict in filtered_data]
+    #create a temp plot
+    figure = px.line(x=dates, y=temperatures,
+                     labels={"x": "Date", "y": "Temperature (C)"})
+    st.plotly_chart(figure)
 
-figure = px.line(x=dates, y=temperatures,
-                 labels={"x": "Date", "y": "Temperatures"})
-
-st.plotly_chart(figure)
+if option == "Sky":
+    # note that weather is a list containing one dict thereofre we reference the index of that single dict with [0].
+    # The main key holds required key, eg Rain, Cloud etc..
+    filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    st.image()
